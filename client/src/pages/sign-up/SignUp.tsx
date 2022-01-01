784c0aa7660bd7c79e8f8096ef883d7698da2b81
shaharKamay <shahar.kamay@gmail.com>
2022-01-01 19:51:34 +0200

    added error handling

--- client/src/pages/sign-up/SignUp.tsx
+++ client/src/pages/sign-up/SignUp.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useContext, useEffect, FormEvent } from "react";
+import React, { useState, useContext, useEffect, FormEvent } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
-import { useNavigate } from "react-router-dom";
-import Form from "../../components/form/Form";
+import { useNavigate } from 'react-router-dom';
+import Form from '../../components/form/Form';
 import '../../assets/styles/sign-up.scss';
 import validator from 'validator';
-import { FormElementType } from "../../types/form";
+import { FormElementType } from '../../types/form';
 
 function SignUp() {
   const authContext = useContext(AuthContext);
@@ -26,10 +26,10 @@ function SignUp() {
 
   const formElements: FormElementType[] = [
     {
-      labelValue: "First Name",
-      type: "text",
-      id: "first-name",
-      placeholder: "Enter first name",
+      labelValue: 'First Name',
+      type: 'text',
+      id: 'first-name',
+      placeholder: 'Enter first name',
       state: firstName,
       setState: setFirstName,
       handleBlur: (e, setError) => {
@@ -37,13 +37,13 @@ function SignUp() {
         if (!validator.isAlpha(e.target.value)) {
           setError('Invalid name');
         } else setError('');
-      }
+      },
     },
     {
-      labelValue: "Last Name",
-      type: "text",
-      id: "last-name",
-      placeholder: "Enter last name",
+      labelValue: 'Last Name',
+      type: 'text',
+      id: 'last-name',
+      placeholder: 'Enter last name',
       state: lastName,
       setState: setLastName,
       handleBlur: (e, setError) => {
@@ -51,13 +51,13 @@ function SignUp() {
         if (!validator.isAlpha(e.target.value)) {
           setError('Invalid name');
         } else setError('');
-      }
+      },
     },
     {
-      labelValue: "Email",
-      type: "email",
-      id: "email",
-      placeholder: "Enter Email",
+      labelValue: 'Email',
+      type: 'email',
+      id: 'email',
+      placeholder: 'Enter Email',
       state: email,
       setState: setEmail,
       handleBlur: (e, setError) => {
@@ -65,21 +65,23 @@ function SignUp() {
         if (!validator.isEmail(e.target.value)) {
           setError('Invalid email');
         } else setError('');
-      }
+      },
     },
     {
-      labelValue: "Password",
-      type: "password",
-      id: "password",
-      placeholder: "Enter Password",
+      labelValue: 'Password',
+      type: 'password',
+      id: 'password',
+      placeholder: 'Enter Password',
       state: password,
       setState: setPassword,
       handleBlur: (e, setError) => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call
         if (!validator.isStrongPassword(e.target.value, { minSymbols: 0 })) {
-          setError('Password must contain at least one uppercase, one lowercase and one number');
+          setError(
+            'Password must contain at least one uppercase, one lowercase and one number'
+          );
         } else setError('');
-      }
+      },
     },
   ];
 
@@ -88,9 +90,8 @@ function SignUp() {
     if (signUp) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
       const data: any = await signUp({ firstName, lastName, email, password });
-      if (data.isSignedUp) navigate('/login');
-      else {
-        // error
+      if (data) {
+        if (data.isSignedUp) navigate('/login');
       }
     }
   };
